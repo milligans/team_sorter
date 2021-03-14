df6e5224cbdec5a1c3cabd282a6b8772156fc5cf
Suzanne Milligan <millgansp@cardiff.ac.uk>
2021-03-14 14:58:47 +0000

    added working questions return

--- main.py
+++ main.py
@@ -5,11 +5,6 @@ student_skills = Questionnaire()
 
 student_skills.build_questions()
 
-print(student_skills.quest_items)
-print(student_skills.answers)
-
-print(len(student_skills.answers))
-
 student_questions = student_skills.quest_items
 student_answers = student_skills.answers
 print(len(student_questions))
